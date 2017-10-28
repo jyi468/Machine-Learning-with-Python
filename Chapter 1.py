68f668625f285d4383d1a2282f9335e946d29343
Josh <jyi468@gmail.com>
2017-10-28 08:21:35 -0400

    Rename file

--- Chapter 1.py
+++ Chapter 1.py
@@ -41,19 +41,27 @@ X_train, X_test, y_train, y_test = train_test_split(iris['data'], iris['target']
 
 # Visualize data. Scatter plot is a common way. You can only display 2 or 3 features usually, so you can do a pair plot
 # Which has pairs of all the features
-fig, ax = plt.subplots(3, 3, figsize=(15, 15))
-plt.suptitle("iris_pairplot")
-for i in range(3):
-    for j in range(3):
-        ax[i, j].scatter(X_train[:, j], X_train[:, i + 1], c=y_train, s=60)
-        ax[i, j].set_xticks(())
-        ax[i, j].set_yticks(())
-        if i == 2:
-            ax[i, j].set_xlabel(iris['feature_names'][j])
-        if j == 0:
-            ax[i, j].set_ylabel(iris['feature_names'][i + 1])
-        if j > i:
-            ax[i, j].set_visible(False)
+# fig, ax = plt.subplots(3, 3, figsize=(15, 15))
+# plt.suptitle("iris_pairplot")
+# for i in range(3):
+#     for j in range(3):
+#         ax[i, j].scatter(X_train[:, j], X_train[:, i + 1], c=y_train, s=60)
+#         ax[i, j].set_xticks(())
+#         ax[i, j].set_yticks(())
+#         if i == 2:
+#             ax[i, j].set_xlabel(iris['feature_names'][j])
+#         if j == 0:
+#             ax[i, j].set_ylabel(iris['feature_names'][i + 1])
+#         if j > i:
+#             ax[i, j].set_visible(False)
+# plt.show()
+
+# create dataframe from data in X_train
+# label the columns using the strings in iris_dataset.feature_names
+iris_dataframe = pd.DataFrame(X_train, columns=iris.feature_names)
+# create a scatter matrix from the dataframe, color by y_train. This will modify the plt object we imported
+grr = pd.plotting.scatter_matrix(iris_dataframe, c=y_train, figsize=(15, 15), marker='o',
+    hist_kwds={'bins': 20}, s=60, alpha=.8)
 # plt.show()
 
 # Plots show relatively well separated classes. We will use k-nearest neighbor classifier to separate out
@@ -74,7 +82,7 @@ print(X_new.shape)
 
 # Predict with knn. Our prediction shows us 0, which means it's Setosa
 prediction = knn.predict(X_new)
-print(prediction)
+print(iris.target_names[prediction])
 
 # Evaluate the model. Use predictions for iris using test data and compare it against its label (the known species)
 # Compute accuracy, fraction of flowers for which the right species was predicted
@@ -86,3 +94,5 @@ print(X_test)
 
 # Can also use knn.score to compute the test set accuracy
 print(knn.score(X_test, y_test))
+
+print(iris)
