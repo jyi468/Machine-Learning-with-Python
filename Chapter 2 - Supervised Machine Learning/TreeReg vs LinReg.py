65a43de6be39787e906f68ce341aa168028adbe9
Joshua YI <joshua.yi@amadeus.com>
2017-11-06 17:54:14 -0500

    Add files

---
+++ Chapter 2 - Supervised Machine Learning/TreeReg vs LinReg.py
@@ -0,0 +1,27 @@
+from sklearn.tree import DecisionTreeRegressor
+import numpy as np
+import matplotlib.pyplot as plt
+import mglearn
+import pandas as pd
+ram_prices = pd.read_csv("data/ram_price.csv")
+
+# use historical data to forecast prices after the year 2000
+data_train = ram_prices[ram_prices.date < 2000]
+data_test = ram_prices[ram_prices.date >= 2000]
+
+# predict prices based on date
+X_train = data_train.date[:, np.newaxis]
+
+# we use a log-transform to get a simpler relationship of data to target
+y_train = np.log(data_train.price)
+tree = DecisionTreeRegressor().fit(X_train, y_train)
+linear_reg = LinearRegression().fit(X_train, y_train)
+
+# predict on all data
+X_all = ram_prices.date[:, np.newaxis]
+pred_tree = tree.predict(X_all)
+pred_lr = linear_reg.predict(X_all)
+
+# undo log-transform
+price_tree = np.exp(pred_tree)
+price_lr = np.exp(pred_lr)
