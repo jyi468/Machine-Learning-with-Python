d20b22c4526da1d1ddba2a2f80837f9d24a520fd
Joshua YI <joshua.yi@amadeus.com>
2017-11-07 17:55:59 -0500

    Add files

---
+++ Chapter 2 - Supervised Machine Learning/Random Forests.py
@@ -0,0 +1,28 @@
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import make_moons
+from sklearn.model_selection import train_test_split
+import numpy as np
+import matplotlib.pyplot as plt
+import mglearn
+import pandas as pd
+
+X, y = make_moons(n_samples=100, noise=0.25, random_state=3)
+X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y,
+    random_state=42)
+
+# Random forest of 5 trees applied to moons dataset
+forest = RandomForestClassifier(n_estimators=5, random_state=2)
+forest.fit(X_train, y_train)
+
+# estimator_ attribute stores trees
+fig, axes = plt.subplots(2, 3, figsize=(20, 10))
+for i, (ax, tree) in enumerate(zip(axes.ravel(), forest.estimators_)):
+    ax.set_title("Tree {}".format(i))
+    mglearn.plots.plot_tree_partition(X_train, y_train, tree, ax=ax)
+
+mglearn.plots.plot_2d_separator(forest, X_train, fill=True, ax=axes[-1, -1],
+    alpha=.4)
+
+axes[-1, -1].set_title("Random Forest")
+mglearn.discrete_scatter(X_train[:, 0], X_train[:, 1], y_train)
+plt.show()
